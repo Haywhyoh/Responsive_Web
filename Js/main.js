6af9f2a1a682454f301f3250f5ec79f5fc0f6700
Haywhyoh <adefeyisayo998@gmail.com>
2021-07-03 19:03:44 +0100

    ''

--- Js/main.js
+++ Js/main.js
@@ -3,6 +3,23 @@ jQuery(document).ready(function(){
     $('.slider').ripples({
         dropRadius: 15,
         perturbance: 0.01,
+    });
+
+    $('.text').typed({
+        strings:['FRONT-END DEVELOPMENT', 'UI/UX DESIGN', 'WORDPRESS'],
+        typespeed:0.5,
+        loop:true
     })
 
+    $(window).scroll(function(){
+        var top =$(window).scrollTop();
+        if(top>=60){
+            $('nav').addClass('secondary');
+        }
+
+        else
+            if($('nav').hasclass('secondary')){
+                $('nav').removeclass('secondary');
+            }
+    })
 })
